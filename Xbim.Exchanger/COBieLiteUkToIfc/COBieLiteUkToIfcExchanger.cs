4bbb0dc81faa35d8a6243f776c799b048b520314
alansmithribae <alan.smith@ribaenterprises.com>
2015-04-02 13:47:55 +0100

    Fixed crash when Facility doesn't have a classificaiton

--- Xbim.Exchanger/COBieLiteUkToIfc/COBieLiteUkToIfcExchanger.cs
+++ Xbim.Exchanger/COBieLiteUkToIfc/COBieLiteUkToIfcExchanger.cs
@@ -63,28 +63,33 @@ namespace XbimExchanger.COBieLiteUkToIfc
         #endregion
 
         #region Static members and functions
-        static readonly IDictionary<string, NamedProperty> CobieToIfcPropertyMap = new Dictionary<string, NamedProperty>();
+        static IDictionary<string, NamedProperty> CobieToIfcPropertyMap = new Dictionary<string, NamedProperty>();
 
 
         static CoBieLiteUkToIfcExchanger()
         {
-
-            var configMap = new ExeConfigurationFileMap { ExeConfigFilename = "COBieAttributes.config" };
-            var config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
-            var cobiePropertyMaps = (AppSettingsSection)config.GetSection("COBiePropertyMaps");
-
-            foreach (KeyValueConfigurationElement keyVal in cobiePropertyMaps.Settings)
+            try
             {
-                var values = keyVal.Value.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                var selected = values.FirstOrDefault();
-                if (selected != null)
+                var configMap = new ExeConfigurationFileMap { ExeConfigFilename = "COBieAttributes.config" };
+                var config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+                var cobiePropertyMaps = (AppSettingsSection)config.GetSection("COBiePropertyMaps");
+
+                foreach (KeyValueConfigurationElement keyVal in cobiePropertyMaps.Settings)
                 {
-                    var names = selected.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-                    if (names.Length == 2)
-                        CobieToIfcPropertyMap.Add(keyVal.Key, new NamedProperty(names[0], names[1]));
+                    var values = keyVal.Value.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    var selected = values.FirstOrDefault();
+                    if (selected != null)
+                    {
+                        var names = selected.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (names.Length == 2)
+                            CobieToIfcPropertyMap.Add(keyVal.Key, new NamedProperty(names[0], names[1]));
+                    }
                 }
             }
-
+            catch
+            {
+                // Carry on
+            }
         }
         #endregion
 
@@ -128,6 +133,39 @@ namespace XbimExchanger.COBieLiteUkToIfc
         #endregion
 
         #region Constructors
+
+        /// <summary>
+        /// Constructs the exchanger - allowing passing in of COBie settings rather than looking for a file relative
+        /// to an Executable
+        /// </summary>
+        /// <param name="facility"></param>
+        /// <param name="repository"></param>
+        /// <param name="configFilePath"></param>
+        public CoBieLiteUkToIfcExchanger(Facility facility, XbimModel repository, string configFilePath)
+            : this(facility, repository)
+        {
+            CobieToIfcPropertyMap = new Dictionary<string, NamedProperty>();
+
+            System.Xml.XmlDocument settings = new System.Xml.XmlDocument();
+            settings.Load(configFilePath);
+
+            var settingsElement = settings.DocumentElement.SelectSingleNode("COBiePropertyMaps");
+            foreach (var element in settingsElement.ChildNodes)
+            {
+                System.Xml.XmlElement el = element as System.Xml.XmlElement;
+
+                if (el == null) continue;
+
+                var values = el.GetAttribute("value").Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                var selected = values.FirstOrDefault();
+                if (selected != null)
+                {
+                    var names = selected.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (names.Length == 2 && !CobieToIfcPropertyMap.ContainsKey(el.GetAttribute("key")))
+                        CobieToIfcPropertyMap.Add(el.GetAttribute("key"), new NamedProperty(names[0], names[1]));
+                }
+            }
+        }
 
         /// <summary>
         /// Constructs the exchanger
