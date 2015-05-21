2e221b5ae1dccfcf70b8a63cbcfd935fd880ef46
alansmithribae <alan.smith@ribaenterprises.com>
2015-05-21 08:17:02 +0100

    Added test to make check whether celings not coming through was a deployment issue or not

--- Tests/COBieLiteUKTests.cs
+++ Tests/COBieLiteUKTests.cs
@@ -605,6 +605,35 @@ namespace Tests
                 }
             }
         }
+        [TestMethod]
+        [DeploymentItem("RIBAETestFiles\\001 BTK Sample.ifc")]
+        public void IfcToCoBieLiteUkTestSingleFile()
+        {
+            string ifcTestFile = "001 BTK Sample.ifc";
+
+            using (var m = new XbimModel())
+            {
+                var xbimTestFile = Path.ChangeExtension(ifcTestFile, "xbim");
+                var jsonFile = Path.ChangeExtension(ifcTestFile, "json");
+                m.CreateFrom(ifcTestFile, xbimTestFile, null, true, true);
+                var facilities = new List<Facility>();
+                var ifcToCoBieLiteUkExchanger = new IfcToCOBieLiteUkExchanger(m, facilities);
+                facilities = ifcToCoBieLiteUkExchanger.Convert();
+
+                foreach (var facilityType in facilities)
+                {
+                    var log = new StringWriter();
+                    facilityType.ValidateUK2012(log, true);
+
+                    string msg;
+                    facilityType.WriteJson(jsonFile, true);
+                    facilityType.WriteCobie("..\\..\\" + System.IO.Path.ChangeExtension(ifcTestFile, ".xlsx"), out msg, "UK2012", true);
+
+
+                    break;
+                }
+            }
+        }
         [DeploymentItem("ValidationFiles\\Lakeside_Restaurant.json")]
         [DeploymentItem("ValidationFiles\\Lakeside_Restaurant-stage6-COBie.json")]
         [TestMethod]
