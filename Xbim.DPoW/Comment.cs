f393c822509fe020964b5526b37ed17df91ace5b
alansmithribae <alan.smith@ribaenterprises.com>
2015-06-09 15:28:23 +0100

    Save test files

---
+++ Xbim.DPoW/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xbim.DPoW
+{
+    public class Comment
+    {
+        public string Content { get; set; }
+        public DateTime LastModified { get; set; }
+        public string UserId { get; set; }
+    }
+}
